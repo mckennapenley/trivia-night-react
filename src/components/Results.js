6ed9b326bf5af87247c0fb77ededcfa00eff6d83
McKenna Penley <mckennapenley@gmail.com>
2021-11-30 17:33:44 -0500

    adds final result ranking after last question

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,17 +2,35 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Results = (props) => {
+  const [teams, setTeams] = useState([]);
   const game_id = props.match.params.game_id;
   useEffect(() => {
     axios
       .post(`http://localhost:3000/api/games/${game_id}/end_game`)
       .then((response) => {
-        console.log(response);
-        // setTeams(response.data.teams);
+        const teams = response.data.teams;
+
+        teams.sort((a, b) => {
+          return b.score - a.score;
+        });
+        setTeams(teams);
       });
   }, []);
 
-  return <div>This is REsults</div>;
+  return (
+    <div>
+      <p>Results</p>
+      <ol>
+        {teams.map((team, index) => {
+          return (
+            <li key={team.id}>
+              {team.name}: {team.score}
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
 };
 
 export default Results;
